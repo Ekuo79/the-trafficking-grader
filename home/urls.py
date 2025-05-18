fc3c5dee7ffba795e7adc535d44e389958c86481
Ekuo79 <ekuo3242@gmail.com>
2025-05-17 21:49:00 -0400

    SHINE logo points to home page

--- home/urls.py
+++ home/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.HomeView.as_view(), name='home'),
     path('home', views.HomeView.as_view()),
     path('authorized', views.AuthorizedView.as_view()),
 ]
