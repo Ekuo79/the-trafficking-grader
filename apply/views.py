02c199ebb053a4244575cb1016d3add70eaed487
Ekuo79 <ekuo3242@gmail.com>
2025-05-19 15:55:28 -0400

    completed apply page and fixed home file retreival

--- apply/views.py
+++ apply/views.py
@@ -3,6 +3,12 @@ from django.http import HttpResponse
 from datetime import datetime
 from django.views.generic import TemplateView
 from django.contrib.auth.mixins import LoginRequiredMixin
+from companies.models import Companies
 
 class ApplyView(TemplateView):
     template_name = 'apply/apply.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['companies'] = Companies.objects.all()
+        return context
